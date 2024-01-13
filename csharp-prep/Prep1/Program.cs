e31442dfbc36291a3bbe163c5f62d2b8f7cdac71
learnado09 <learnado09@gmail.com>
2024-01-12 17:25:48 -0700

    fixed and error in my code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,10 @@
 using System;
 
-class program 
+class Program
 {
     static void Main(string[] args)
     {
+    
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
